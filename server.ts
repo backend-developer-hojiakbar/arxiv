c8c617da6b88ccc103a95e6959caa3d202642786
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2026-06-06 08:21:44 +0500

    first commits

--- server.ts
+++ server.ts
@@ -6,12 +6,8 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
-import { fileURLToPath } from "url";
 import { createServer as createViteServer } from "vite";
 import { UserRole, DocumentStatus } from "./src/types.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const DB_PATH = path.join(process.cwd(), "db.json");
